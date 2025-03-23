9f3049b8ad99b4e0977b3823186228b4b7c42c2b
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-03-23 19:08:06 +0300

    Added Income & Expense models

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Income(models.Model):
+    source = models.CharField(max_length=100)  
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.source} - {self.amount}"
+
+class Expense(models.Model):
+    category = models.CharField(max_length=100)  
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.category} - {self.amount}"
+
