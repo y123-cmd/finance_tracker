1b8f68e39b6a8a5ab8534aae24650b98a5eba10a
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-04-02 11:40:11 +0300

    Updated settings, views, and templates

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "tracker"
+
+
 urlpatterns = [
+    path('', views.home, name='home'),
     path('income/', views.income_list, name='income_list'),
+    path("income/add/", views.add_income, name="add_income"),
     path('expense/', views.expense_list, name='expense_list'),
+    path("expenses/add/", views.add_expense, name="add_expense"),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -9,3 +9,11 @@ def expense_list(request):
     expenses = Expense.objects.all()  
     return render(request, 'tracker/expense_list.html', {'expenses': expenses})
 
+def home(request):
+    return render(request, 'base.html')
+
+def add_income(request):
+    return render(request, 'tracker/add_income.html')
+
+def add_expense(request):
+    return render(request, 'tracker/add_expense.html')
