daf051b834341aa13fcedfefe876133cddf5efbb
Yvonne <81848589+y123-cmd@users.noreply.github.com>
2025-04-17 01:00:53 +0300

    added the delete and update button

--- tracker/views.py
+++ tracker/views.py
@@ -3,6 +3,7 @@ from .models import Income, Expense
 from .forms import ExpenseForm
 from django.shortcuts import render, redirect
 from .forms import IncomeForm
+from django.shortcuts import get_object_or_404
 
 def income_list(request):
     incomes = Income.objects.all()  
@@ -36,3 +37,38 @@ def add_expense(request):
         form = ExpenseForm()  
     
     return render(request, 'tracker/add_expense.html', {'form': form})
+def update_income(request, income_id):
+    income = get_object_or_404(Income, pk=income_id)
+    if request.method == 'POST':
+        form = IncomeForm(request.POST, instance=income)
+        if form.is_valid():
+            form.save()
+            return redirect('tracker:income_list')
+    else:
+        form = IncomeForm(instance=income)
+    return render(request, 'tracker/add_income.html', {'form': form})
+
+def delete_income(request, id):
+    income = get_object_or_404(Income, pk=income_id)
+    if request.method == 'POST':
+        income.delete()
+        return redirect('tracker:income_list')
+    return render(request, 'tracker/confirm_delete.html', {'item': income, 'type': 'Income'})
+
+def update_expense(request, expense_id):
+    expense = get_object_or_404(Expense, pk=expense_id)
+    if request.method == 'POST':
+        form = ExpenseForm(request.POST, instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('tracker:expense_list')
+    else:
+        form = ExpenseForm(instance=expense)
+    return render(request, 'tracker/add_expense.html', {'form': form})
+
+def delete_expense(request, expense_id):
+    expense = get_object_or_404(Expense, pk=expense_id)
+    if request.method == 'POST':
+        expense.delete()
+        return redirect('tracker:expense_list')
+    return render(request, 'tracker/confirm_delete.html', {'item': expense, 'type': 'Expense'})
